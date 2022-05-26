b74dbde7b7ec0daf575220366416c475aced0092
bamsi <hibamsi@gmail.com>
2022-05-27 00:09:32 +0300

    add state to calculator component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,44 @@
-import React from "react";
+/* eslint react/prefer-stateless-function: [0] */
+
+import React from 'react';
 
 class Calculator extends React.Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { result: 0 };
+  }
 
-    render(){
-        return (
-         <div className="wrapper">
-           <div className="result"> 0 </div>
-           <div> AC </div>
-           <div> +/- </div>
-           <div> % </div>
-           <div className="operation"> ÷ </div>
-           <div> 7 </div>
-           <div> 8 </div>
-           <div> 9 </div>
-           <div className="operation"> × </div>
-           <div> 4 </div>
-           <div> 5 </div>
-           <div> 6 </div>
-           <div className="operation"> - </div>
-           <div> 1 </div>
-           <div> 2 </div>
-           <div> 3 </div>
-           <div className="operation"> + </div>
-           <div className="zero"> 0 </div>
-           <div> . </div>
-           <div className="operation"> = </div>
-         </div>
-        );
-    }
+  render() {
+    const { result } = this.state;
+    return (
+      <div className="wrapper">
+        <div className="result">
+          {' '}
+          { result}
+          {' '}
+        </div>
+        <div> AC </div>
+        <div> +/- </div>
+        <div> % </div>
+        <div className="operation"> ÷ </div>
+        <div> 7 </div>
+        <div> 8 </div>
+        <div> 9 </div>
+        <div className="operation"> × </div>
+        <div> 4 </div>
+        <div> 5 </div>
+        <div> 6 </div>
+        <div className="operation"> - </div>
+        <div> 1 </div>
+        <div> 2 </div>
+        <div> 3 </div>
+        <div className="operation"> + </div>
+        <div className="zero"> 0 </div>
+        <div> . </div>
+        <div className="operation"> = </div>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
