6a172e8e378f169429ace34d9b8f16008c41eb5d
bamsi <hibamsi@gmail.com>
2022-05-28 01:37:48 +0300

    refactor project using function component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,61 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onClickCall = this.onClickCall.bind(this);
-  }
-
-  onClickCall = (e) => {
-    const { clickHandler } = this.props;
+const Calculator = ({
+  total, operation, next, clickHandler,
+}) => {
+  const onClickCall = (e) => {
     clickHandler(e.target.value);
-  }
+  };
 
-  render() {
-    const { total } = this.props;
-    const { next } = this.props;
-    const { operation } = this.props;
-    const buttons = [
-      { style: '', value: 'AC' },
-      { style: '', value: '+/-' },
-      { style: '', value: '%' },
-      { style: 'operation', value: '÷' },
-      { style: '', value: '7' },
-      { style: '', value: '8' },
-      { style: '', value: '9' },
-      { style: 'operation', value: 'x' },
-      { style: '', value: '4' },
-      { style: '', value: '5' },
-      { style: '', value: '6' },
-      { style: 'operation', value: '-' },
-      { style: '', value: '1' },
-      { style: '', value: '2' },
-      { style: '', value: '3' },
-      { style: 'operation', value: '+' },
-      { style: 'zero', value: '0' },
-      { style: '', value: '.' },
-      { style: 'operation', value: '=' },
-    ];
-    const items = [];
-    buttons.forEach((button) => {
-      items.push(<button type="button" value={button.value} className={button.style} onClick={this.onClickCall}>{button.value}</button>);
-    });
-    return (
-      <div className="wrapper">
-        <button className="result" type="button">
-          { total }
-          { operation }
-          { next }
-        </button>
-        {items}
-      </div>
+  const buttons = [
+    { style: '', value: 'AC' },
+    { style: '', value: '+/-' },
+    { style: '', value: '%' },
+    { style: 'operation', value: '÷' },
+    { style: '', value: '7' },
+    { style: '', value: '8' },
+    { style: '', value: '9' },
+    { style: 'operation', value: 'x' },
+    { style: '', value: '4' },
+    { style: '', value: '5' },
+    { style: '', value: '6' },
+    { style: 'operation', value: '-' },
+    { style: '', value: '1' },
+    { style: '', value: '2' },
+    { style: '', value: '3' },
+    { style: 'operation', value: '+' },
+    { style: 'zero', value: '0' },
+    { style: '', value: '.' },
+    { style: 'operation', value: '=' },
+  ];
+  const items = [];
+  buttons.forEach((button) => {
+    items.push(
+      <button
+        type="button"
+        value={button.value}
+        className={button.style}
+        onClick={onClickCall}
+      >
+        {button.value}
+      </button>,
     );
-  }
-}
+  });
+  return (
+    <div className="wrapper">
+      <button className="result" type="button">
+        {total}
+        {operation}
+        {next}
+      </button>
+      {items}
+    </div>
+  );
+};
 
 Calculator.propTypes = {
-  total: PropTypes.number,
+  total: PropTypes.string,
   next: PropTypes.string,
   operation: PropTypes.string,
   clickHandler: PropTypes.func.isRequired,
@@ -66,5 +66,4 @@ Calculator.defaultProps = {
   next: null,
   operation: null,
 };
-
 export default Calculator;
