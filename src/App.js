7b9e550d83bc429478e6e88154645d0bbbc633e0
bamsi <hibamsi@gmail.com>
2022-05-27 23:41:46 +0300

    implement event to calculator button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 /* eslint react/prefer-stateless-function: [0] */
+/* eslint-disable react/jsx-props-no-spreading */
 
 import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
+import calculate from './components/logic/calculate';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: '',
+      operation: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const data = calculate(this.state, e);
+    this.setState(data);
+  }
+
   render() {
     return (
-      <Calculator />
+      <Calculator clickHandler={this.handleChange} {...this.state} />
     );
   }
 }
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,44 +1,65 @@
 /* eslint react/prefer-stateless-function: [0] */
+/* eslint linebreak-style: ["error", "windows"] */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0 };
+    this.onClickCall = this.onClickCall.bind(this);
+  }
+
+  onClickCall = (e) => {
+    const { clickHandler } = this.props;
+    clickHandler(e.target.value);
   }
 
   render() {
-    const { result } = this.state;
+    const { total } = this.props;
+    const buttons = [
+      { style: '', value: 'AC' },
+      { style: '', value: '+/-' },
+      { style: '', value: '%' },
+      { style: 'operation', value: '÷' },
+      { style: '', value: '7' },
+      { style: '', value: '8' },
+      { style: '', value: '9' },
+      { style: 'operation', value: 'x' },
+      { style: '', value: '4' },
+      { style: '', value: '5' },
+      { style: '', value: '6' },
+      { style: 'operation', value: '-' },
+      { style: '', value: '1' },
+      { style: '', value: '2' },
+      { style: '', value: '3' },
+      { style: 'operation', value: '+' },
+      { style: 'zero', value: '0' },
+      { style: '', value: '.' },
+      { style: 'operation', value: '=' },
+    ];
+    const items = [];
+    buttons.forEach((button) => {
+      items.push(<button type="button" value={button.value} className={button.style} onClick={this.onClickCall}>{button.value}</button>);
+    });
     return (
       <div className="wrapper">
-        <div className="result">
-          {' '}
-          { result}
-          {' '}
-        </div>
-        <div> AC </div>
-        <div> +/- </div>
-        <div> % </div>
-        <div className="operation"> ÷ </div>
-        <div> 7 </div>
-        <div> 8 </div>
-        <div> 9 </div>
-        <div className="operation"> × </div>
-        <div> 4 </div>
-        <div> 5 </div>
-        <div> 6 </div>
-        <div className="operation"> - </div>
-        <div> 1 </div>
-        <div> 2 </div>
-        <div> 3 </div>
-        <div className="operation"> + </div>
-        <div className="zero"> 0 </div>
-        <div> . </div>
-        <div className="operation"> = </div>
+        <button className="result" type="button">
+          {total || 0}
+        </button>
+        {items}
       </div>
     );
   }
 }
+
+Calculator.propTypes = {
+  total: PropTypes.number,
+  clickHandler: PropTypes.func.isRequired,
+};
+
+Calculator.defaultProps = {
+  total: 0,
+};
 
 export default Calculator;
