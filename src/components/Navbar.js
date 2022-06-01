b08b0fd83dbeb83f12fa5849de93ef0e2cc0860b
bamsi <hibamsi@gmail.com>
2022-06-02 00:04:41 +0300

    add navbar page

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+          <link to="/">Home</link>
+          <link to="/calculator">Calculator</link>
+          <link to="/quote">Quote</link>
+        </div>
+    )
+}
