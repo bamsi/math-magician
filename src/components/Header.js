3cc5b4f81f7f3a090d72eb6322a730faa7ce6ae5
bamsi <hibamsi@gmail.com>
2022-06-02 00:17:32 +0300

    add header section of the page

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Math Magicians</h1>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
